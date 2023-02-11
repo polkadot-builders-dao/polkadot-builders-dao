f5b45a52e3be264533956bcdd821e3fca537f034
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-02-11 22:23:12 +0900

    test ci

--- web/src/lib/settings.ts
+++ web/src/lib/settings.ts
@@ -1,3 +1,1 @@
-import { supportedChains } from "./wagmi/supportedChains"
-
-export const CHAIN_ID = Number(import.meta.env.VITE_CHAIN_ID as string) as 31337 // | 1287 | 1284
+export const CHAIN_ID = Number(import.meta.env.VITE_CHAIN_ID as string) as 1287 // | 1287 | 1284

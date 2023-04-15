8f22efe739a38a5b5b56ca4ec2bd9c70e2768803
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-04-15 12:16:49 +0900

    fix: squid

--- squids/polkadot-builders/db/migrations/1681528588156-Data.js
+++ squids/polkadot-builders/db/migrations/1681528588156-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1681527848488 {
-    name = 'Data1681527848488'
+module.exports = class Data1681528588156 {
+    name = 'Data1681528588156'
 
     async up(db) {
         await db.query(`CREATE TABLE "owner" ("id" character varying NOT NULL, CONSTRAINT "PK_8e86b6b9f94aece7d12d465dc0c" PRIMARY KEY ("id"))`)

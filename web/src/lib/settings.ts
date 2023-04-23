fa4ad0dd86240283505979c7e5e4148cde9199a5
Kheops <26880866+0xKheops@users.noreply.github.com>
2023-04-23 16:44:39 +0900

    feat: prod squid v3

--- web/src/lib/settings.ts
+++ web/src/lib/settings.ts
@@ -7,7 +7,7 @@ export const CHAIN_ID = Number(
 const SQUID_URLS = {
   31337: "http://localhost:4350/graphql",
   1287: "https://squid.subsquid.io/polkadot-builders-moonbase/v/v2/graphql",
-  1284: "https://squid.subsquid.io/polkadot-builders/v/v2/graphql",
+  1284: "https://squid.subsquid.io/polkadot-builders/v/v3/graphql",
 }
 
 export const SQUID_URL = SQUID_URLS[CHAIN_ID]
